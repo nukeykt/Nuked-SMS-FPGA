f58211ce06e7e88a7b886736ee7b033a742b2b86
nukeykt <alexeytf2@gmail.com>
2023-10-28 23:52:15 +0900

    gpl headers

--- sega315_5216.v
+++ sega315_5216.v
@@ -1,3 +1,26 @@
+/*
+ * Copyright (C) 2023 nukeykt
+ *
+ * This file is part of Nuked-SMS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  Sega 315-5216 emulator
+ *  Thanks:
+ *      Furrtek:
+ *          Sega 315-5216 decap & die shot.
+ *      org, andkorzh, HardWareMan (emu-russia):
+ *          help & support.
+ */
+
 // 315-5216, fujitsu variant
 
 module sega315_5216
